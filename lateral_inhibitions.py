ee28d7299b0625f27fd8e4bad67a49acd4257967
woitee <woitee@seznam.cz>
2016-02-29 16:37:38 +0100

    Added Euclidian distance for neurons in grid

--- lateral_inhibitions.py
+++ lateral_inhibitions.py
@@ -1,9 +1,12 @@
 import math
 from operator import sub
 
-# Manhattan distance... TODO: Go Euclidian
-def __id_dist(id1, id2):
+def __id_man_dist(id1, id2):
     return sum(map(abs, map(sub, id1, id2)))
+
+
+def __id_euc_dist(id1, id2):
+    return math.sqrt(sum(map(lambda x, y: (x - y)**2, id1, id2)))
 
 
 def hex_loc(id):
@@ -12,8 +15,8 @@ def hex_loc(id):
 
 
 def gauss(id1, id2, diameter):
-    return math.e**-(__id_dist(id1, id2) / diameter)**2
+    return math.e**-(__id_euc_dist(id1, id2) / diameter)**2
 
 
 def gauss_hex(id1, id2, diameter):
-    return math.e**-(__id_dist(hex_loc(id1), hex_loc(id2)) / diameter)**2
+    return math.e**-(__id_euc_dist(hex_loc(id1), hex_loc(id2)) / diameter)**2
